7615ecf6ae629887dce0bf83183ba55438a00df4
hamtolchu <seungjaey@gmail.com>
2023-03-23 19:00:04 +0900

    for 준모

---
+++ pages/font/index.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from 'react';
+import { pipe, map, range, toArray, size, isEmpty, nth } from '@fxts/core';
+import { FixedSizeList } from 'react-window';
+
+const UnicodeChar = ({ index, style }) => {
+  return <div style={{ fontFamily: 'Noto Sans' }}>{}</div>;
+};
+const getFont = async () => {
+  const font = new FontFace('Noto Sans', "url('https://res.kurly.com/fonts/NotoSansKR-Light.woff2')", {
+    weight: 300,
+    style: 'normal',
+  });
+  await font.load();
+  return font;
+};
+
+interface UnicodeItem {
+  code: number;
+  char: string;
+}
+const FontPage = () => {
+  const [unicodeRangeArr, setUnicodeRangeArr] = useState<UnicodeItem[]>([]);
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+    (async () => {
+      const font = await getFont();
+      const rangeText = font.unicodeRange.replace(/^(U+)/, '');
+      const [start, end] = pipe(
+        rangeText.split('-') as string[],
+        map((text) => parseInt(text, 16)),
+        toArray,
+      );
+
+      const result = pipe(
+        range(start, end),
+        map((code) => ({
+          code,
+          char: String.fromCharCode(code),
+        })),
+        toArray,
+      );
+
+      setUnicodeRangeArr(result);
+    })();
+  }, []);
+  console.log(unicodeRangeArr);
+  const Row = ({ index, style }) => {
+    const { code, char } = nth(index, unicodeRangeArr);
+    return <div style={style}>{`[${code}] : ${char}`}</div>;
+  };
+
+  return (
+    <div>
+      {!isEmpty(unicodeRangeArr) ? (
+        <FixedSizeList itemSize={35} height={500} itemCount={size(unicodeRangeArr)} width={500}>
+          {Row}
+        </FixedSizeList>
+      ) : null}
+    </div>
+  );
+};
+
+export default FontPage;
